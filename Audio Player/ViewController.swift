0c14d88cf12bf85bee6a8f95606214630140f326
Dmitriy <skokdmitriy@gmail.com>
2023-12-01 22:27:54 +0700

    Initial Commit

---
+++ Audio Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Audio Player
+//
+//  Created by Дмитрий Скок on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
